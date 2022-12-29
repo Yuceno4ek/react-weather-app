19cbcb1fe9abc1c3ecbeaf67d49d42fd41d07365
Yuceno4ek <yuceno4ek@gmail.com>
2022-12-29 15:58:56 +0200

    Added a new styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-
+import React from "react";
+import "./Weather.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <button className="btn btn-primary">Hello</button>
-      <footer>This project was coded by Yuliia Demchenko </footer>
-      <a
-        href="https://github.com/Yuceno4ek/react-weather-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Open source code
-      </a>
+      <footer>
+        This project was coded by Yuliia Demchenko{" "}
+        <a
+          href="https://github.com/Yuceno4ek/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>
+      </footer>
     </div>
   );
 }
